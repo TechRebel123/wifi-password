f30610701bb92f6678218b015fd783fa57ccc390
Dragon-1645 <145196893+Dragon-1645@users.noreply.github.com>
2023-09-16 15:48:19 +0530

    Add files via upload

---
+++ wifi-password/wifi-password.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "==================================================================================="
+figlet -f slant -c wifi-hacking | lolcat
+echo "===================================================================================="
+echo "Created by Dragon"
+
+sleep 3
+
+echo "Wait for 5 seconds, You will get the passwords of the top 3 Wifi-Networks in your area"
+
+sleep 5
+
+sudo apt update
+sudo apt upgrade
+
+clear
+
+echo "==================================================================================="
+figlet -f slant -c wifi-hacking | lolcat
+echo "==================================================================================="
+echo "Created by Dragon"
+
+sleep 2
+
+echo "Wait for 5 seconds"
+
+sleep 5
+echo "==================================================================================="
+figlet -c Success, Here are the passwords: | lolcat
+echo "==================================================================================="
+sleep 5
+echo "Passwords hacked at $(date)"
+sleep 5
+echo "------------------------------------------------------------------------------------"
+echo "wifi 1 = 9836243672"
+echo "wifi 2 = 12345678"
+echo "wifi 3 = mysmallbaby"
+echo "------------------------------------------------------------------------------------"
+
+sleep 5
+
+echo "Dont forget to leave your opinions"
+
+
+
